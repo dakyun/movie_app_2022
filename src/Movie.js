d0b38170bab37fc1341acf0c1dffe124184c8ec2
dakyun <dkdk_21@naver.com>
2022-04-19 11:37:59 +0900

    06-2 깃허브에 리액트 앱 업로드하기

--- src/Movie.js
+++ src/Movie.js
@@ -1,12 +1,19 @@
-import React from 'React';
+import React from 'react';
 import propTypes from 'prop-types';
+import "./Movie.css";
 
-function Movie(){
-    return <h1></h1>
+function Movie({title,year,summary,poster}){
+    return (
+        <div className='movie__data'>
+            <img src={poster} alt={title} />
+            <h3 className='movie__title'>{title}</h3>
+            <h5 className='movie__year'>{year}</h5>
+            <p className='movie__summary'>{summary}</p>
+        </div>
+    )
 }
 
 Movie.propTypes={
-    id:propTypes.number.isRequired,
     year:propTypes.number.isRequired,
     title:propTypes.string.isRequired,
     summary:propTypes.string.isRequired,
