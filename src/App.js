23f9510b87f02ee536690645df182ef79c4c2f9f
dakyun <dkdk_21@naver.com>
2022-04-22 11:14:01 +0900

    리액트 영화앱 1차 완성

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
-import Home from "./routes/Home";
 import {HashRouter, Route} from "react-router-dom";
+import Home from "./routes/Home";
 import About from "./routes/About";
+import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
 
 import "./App.css";
@@ -13,6 +14,7 @@ function App(){
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   )
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import propTypes from 'prop-types';
 import "./Movie.css";
+import {Link} from 'react-router-dom';
 import { string } from 'prop-types';
 
 function Movie({title,year,summary,poster,genres}){
     return (
         <div className='movie__data'>
-            <img src={poster} alt={title} />
-            <h3 className='movie__title'>{title}</h3>
-            <h5 className='movie__year'>{year}</h5>
-            <ul className="movie__genres">
-                {genres.map((genre,index)=>{
-                    return <li key={index} className='movie__genre'>{genre}</li>
-                })}
-            </ul>
-            <p className='movie__summary'>{summary.slice(0,180)}...</p>
+            <Link to={{pathname:'/movie-detail', state:{year,title,summary,poster,genres}}}>
+                <img src={poster} alt={title} />
+                <h3 className='movie__title'>{title}</h3>
+                <h5 className='movie__year'>{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genre,index)=>{
+                        return <li key={index} className='movie__genre'>{genre}</li>
+                    })}
+                </ul>
+                <p className='movie__summary'>{summary.slice(0,180)}...</p>
+            </Link>
         </div>
     )
 }
