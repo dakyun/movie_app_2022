97e7057639b95fc2791b60197b42dc1ddecf615d
dakyun <dkdk_21@naver.com>
2022-05-02 10:47:11 +0900

    어바웃,디테일페이지 css 수정

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
 import "./Navigation.css"
 
 function Navigation(){
+    const [scrollPosition, setScrollPosition] = useState(0);
+    const updateScroll = () => {
+        setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll', updateScroll);
+    });
     return (
         <div className="nav">
-            <Link to="/"><span className="nav_1">홈</span></Link>
-            <div class="gnb">
-                <Link to="/about" className="nav_2">ABOUT</Link>
-                <Link to="#" className="nav_2">MOVIES</Link>
-                <Link to="#" className="nav_2">EVENTS</Link>
-                <Link to="#" className="nav_2">MY PAGE</Link>
+            <div className={scrollPosition < 150 ? "original_header" : "change_header"}>
+                <Link to="/"><span className="nav_1">홈</span></Link>
+                <div class="gnb">
+                    <Link to="/about" className="nav_2">ABOUT</Link>
+                    <Link to="#" className="nav_2">MOVIES</Link>
+                    <Link to="#" className="nav_2">EVENTS</Link>
+                    <Link to="#" className="nav_2">MY PAGE</Link>
+                </div>
+                <div class="search"><a href="#" className="nav_2">검색</a></div>
             </div>
-            <div class="search"><a href="#" className="nav_2">검색</a></div>
         </div>
     )
 }
